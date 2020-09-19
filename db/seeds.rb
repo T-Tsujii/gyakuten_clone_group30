51ab675f5a83575e49be48c0bdf1dd1b0b9c947a
izumi <zzznozu@gmail.com>
2020-09-19 12:53:03 +0900

    14_管理者画面の実装(途中経過）

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@ User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザの初期データインポートに成功しました。'
 end
+
+AdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
