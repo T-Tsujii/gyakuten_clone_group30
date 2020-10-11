cdaeb2da7a17e9a35394211010442ef52fa3634d
hisadomi <hb.kmaia93@icloud.com>
2020-10-11 18:32:23 +0900

    csvデータインポート機能実装

--- db/migrate/20201011085006_create_lines.rb
+++ db/migrate/20201011085006_create_lines.rb
@@ -2,7 +2,7 @@ class CreateLines < ActiveRecord::Migration[6.0]
   def change
     create_table :lines do |t|
       t.string :genre
-      t.string, :title
+      t.string :title
       t.string :content
 
       t.timestamps
