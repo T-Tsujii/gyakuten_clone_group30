c9144a32a7a666fad5f21af53dd52afae0b684fc
maoslele <m01.sgwk@gmail.com>
2020-10-15 23:35:24 +0900

    アカウント編集のリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+  resources :users,  only:[:edit]
   root to: 'movies#index'
   resources 'aws_texts' , only:[:index, :show]
 end
