3e7d39c1bd368ab3c1a2bbf337eb737ce19a6bd4
AkikoMatsui <akk8989162@gmail.com>
2020-10-20 20:57:42 +0900

    質問の新規投稿機能を作成しました

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+  def new
+    @question = Question.new
+  end
+
+  def create
+    Question.create
+    redirect_to new_question_path
+  end
+
+  private
+  def user_params
+    params.require(:question).permit(:title, :detail)
+  end
+
 end
